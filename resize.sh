48fe20ed837e55bf2ca905876757658ea6021725
Gene Gotimer <otherdevopsgene@portinfo.com>
2023-11-10 18:20:36 -0500

    Added resizing Cloud9 script

---
+++ resize.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Usage:
+#   bash resize.sh <size, default is 20>
+
+size=${1:-20}
+class=$(terraform output -raw class_name)
+
+for vol in $(aws ec2 describe-instances --filters "Name=tag:Class,Values=${class}" | jq -r '.Reservations[].Instances[].BlockDeviceMappings[].Ebs.VolumeId'); do
+  aws ec2 modify-volume --volume-id ${vol} --size ${size}
+done
