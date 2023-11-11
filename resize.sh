d95593181f0ecde862332a5f8dbaa4f4af30e225
Gene Gotimer <otherdevopsgene@portinfo.com>
2023-11-11 12:02:09 -0500

    reboot so volume increase takes affect

--- resize.sh
+++ resize.sh
@@ -9,3 +9,5 @@ class=$(terraform output -raw class_name)
 for vol in $(aws ec2 describe-instances --filters "Name=tag:Class,Values=${class}" | jq -r '.Reservations[].Instances[].BlockDeviceMappings[].Ebs.VolumeId'); do
   aws ec2 modify-volume --volume-id ${vol} --size ${size}
 done
+
+aws ec2 reboot-instances --instance-ids $(aws ec2 describe-instances --filters "Name=tag:Class,Values=${class}" | jq -r '.Reservations[].Instances[].InstanceId')
