6e0547c69696dc5faad9f60b95f222c3dad53d2c
Gene Gotimer <otherdevopsgene@portinfo.com>
2024-11-16 13:33:01 -0500

    fix: make resize.sh idempotent

--- resize.sh
+++ resize.sh
@@ -3,11 +3,26 @@
 # Usage:
 #   bash resize.sh <size, default is 20>
 
+start_size=10
 size=${1:-20}
 class=$(terraform output -raw class_name)
 
-for vol in $(aws ec2 describe-instances --filters "Name=tag:Class,Values=${class}" --region us-east-2 | jq -r '.Reservations[].Instances[].BlockDeviceMappings[].Ebs.VolumeId'); do
-  aws ec2 modify-volume --volume-id ${vol} --size ${size} --region us-east-2
-done
+vols=$(aws ec2 describe-instances --filters "Name=tag:Class,Values=${class}" | \
+       jq -r '.Reservations[].Instances[].BlockDeviceMappings[].Ebs.VolumeId' | \
+       xargs aws ec2 describe-volumes --filters "Name=size,Values=${start_size}" --volume-ids | \
+       jq -r '.Volumes[].VolumeId')
 
-aws ec2 reboot-instances --region us-east-2 --instance-ids $(aws ec2 describe-instances --filters "Name=tag:Class,Values=${class}" --region us-east-2 | jq -r '.Reservations[].Instances[].InstanceId')
+if [[ ! -z ${vols[@]} ]]; then
+  for vol in ${vols[@]}; do
+    aws ec2 modify-volume --volume-id ${vol} --size ${size}
+  done
+
+  reboots=$(aws ec2 describe-volumes --volume-ids ${vols[@]} | jq -r '.Volumes[].Attachments[].InstanceId')
+  if [[ ! -z ${reboots[@]} ]]; then
+    aws ec2 reboot-instances --instance-ids ${reboots[@]}
+  else
+    echo "None to reboot."
+  fi
+else
+  echo "None to resize."
+fi
