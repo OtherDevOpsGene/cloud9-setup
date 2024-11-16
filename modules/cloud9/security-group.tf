1f0b1a25bc4affbf98cd8618ec3f03737e6473f9
Gene Gotimer <otherdevopsgene@portinfo.com>
2024-11-16 15:02:27 -0500

    fix: kubectl permissions, drop selenium

--- modules/cloud9/security-group.tf
+++ modules/cloud9/security-group.tf
@@ -23,18 +23,26 @@ resource "aws_vpc_security_group_ingress_rule" "http" {
   to_port           = 80
 }
 
-resource "aws_vpc_security_group_ingress_rule" "http8080" {
-  security_group_id = data.aws_security_group.cloud9.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 8080
-  ip_protocol       = "tcp"
-  to_port           = 8080
-}
+# resource "aws_vpc_security_group_ingress_rule" "http8080" {
+#   security_group_id = data.aws_security_group.cloud9.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 8080
+#   ip_protocol       = "tcp"
+#   to_port           = 8080
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "selenium_grid" {
+# resource "aws_vpc_security_group_ingress_rule" "selenium_grid" {
+#   security_group_id = data.aws_security_group.cloud9.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 4444
+#   ip_protocol       = "tcp"
+#   to_port           = 4444
+# }
+
+resource "aws_vpc_security_group_ingress_rule" "kubectl" {
   security_group_id = data.aws_security_group.cloud9.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 4444
+  from_port         = 6443
   ip_protocol       = "tcp"
-  to_port           = 4444
+  to_port           = 6443
 }
