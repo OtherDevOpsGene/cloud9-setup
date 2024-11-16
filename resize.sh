1c63d0239fbcc987c3979036fb9301f57ce6a01a
Gene Gotimer <otherdevopsgene@portinfo.com>
2024-11-16 13:52:20 -0500

    fix: wait for resize to finish, better messages

--- resize.sh
+++ resize.sh
@@ -13,16 +13,25 @@ vols=$(aws ec2 describe-instances --filters "Name=tag:Class,Values=${class}" | \
        jq -r '.Volumes[].VolumeId')
 
 if [[ ! -z ${vols[@]} ]]; then
+  echo && echo "Resizing..."
   for vol in ${vols[@]}; do
-    aws ec2 modify-volume --volume-id ${vol} --size ${size}
+    aws ec2 modify-volume --volume-id ${vol} --size ${size} | jq -r '.VolumeModification.VolumeId'
   done
 
-  reboots=$(aws ec2 describe-volumes --volume-ids ${vols[@]} | jq -r '.Volumes[].Attachments[].InstanceId')
-  if [[ ! -z ${reboots[@]} ]]; then
-    aws ec2 reboot-instances --instance-ids ${reboots[@]}
+  while [[ ! -z $(aws ec2 describe-volumes-modifications --volume-ids ${vols[@]} \
+                  --filters "Name=modification-state,Values=modifying" | \
+                  jq -r '.VolumesModifications[].VolumeId') ]]; do
+    echo && echo "Waiting for modifications to complete."
+    sleep 1
+  done
+
+  changed=$(aws ec2 describe-volumes --volume-ids ${vols[@]} | jq -r '.Volumes[].Attachments[].InstanceId')
+  if [[ ! -z ${changed[@]} ]]; then
+    echo && echo "Rebooting..."
+    aws ec2 reboot-instances --instance-ids ${changed[@]}
   else
-    echo "None to reboot."
+    echo && echo "None to reboot."
   fi
 else
-  echo "None to resize."
+  echo && echo "None to resize."
 fi
