eb53d5f8218c9f6d993b36fcc393a71b28fe623f
Gene Gotimer <otherdevopsgene@portinfo.com>
2025-01-01 18:31:12 -0500

    feat: NodePort and a non-priv web port

--- modules/cloud9/security-group.tf
+++ modules/cloud9/security-group.tf
@@ -11,38 +11,46 @@ resource "aws_vpc_security_group_ingress_rule" "https" {
   security_group_id = data.aws_security_group.cloud9.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
-  ip_protocol       = "tcp"
   to_port           = 443
+  ip_protocol       = "tcp"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "http" {
   security_group_id = data.aws_security_group.cloud9.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
-  ip_protocol       = "tcp"
   to_port           = 80
+  ip_protocol       = "tcp"
 }
 
-# resource "aws_vpc_security_group_ingress_rule" "http8080" {
-#   security_group_id = data.aws_security_group.cloud9.id
-#   cidr_ipv4         = "0.0.0.0/0"
-#   from_port         = 8080
-#   ip_protocol       = "tcp"
-#   to_port           = 8080
-# }
+resource "aws_vpc_security_group_ingress_rule" "http8080" {
+  security_group_id = data.aws_security_group.cloud9.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  to_port           = 8080
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "nodeport" {
+  security_group_id = data.aws_security_group.cloud9.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 30000
+  to_port           = 32768
+  ip_protocol       = "tcp"
+}
 
 # resource "aws_vpc_security_group_ingress_rule" "selenium_grid" {
 #   security_group_id = data.aws_security_group.cloud9.id
 #   cidr_ipv4         = "0.0.0.0/0"
 #   from_port         = 4444
-#   ip_protocol       = "tcp"
 #   to_port           = 4444
+#   ip_protocol       = "tcp"
 # }
 
 resource "aws_vpc_security_group_ingress_rule" "kubectl" {
   security_group_id = data.aws_security_group.cloud9.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 6443
-  ip_protocol       = "tcp"
   to_port           = 6443
+  ip_protocol       = "tcp"
 }
